40b6a9d3f9f8ed5a0c4449393c4b91b779cd7d04
egorn <egornurkanov31@gmail.com>
2023-06-16 03:01:18 +0300

    Initial commit

---
+++ src/main/java/net/egorplaytv/minecraft_ores/MinecraftOres.java
@@ -0,0 +1,28 @@
+package net.egorplaytv.minecraft_ores;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+import static net.egorplaytv.minecraft_ores.MinecraftOres.MOD_ID;
+
+@Mod(MOD_ID)
+public class MinecraftOres{
+    public static final String MOD_ID = "minecraft_ores";
+
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MinecraftOres(){
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event){
+    }
+}
